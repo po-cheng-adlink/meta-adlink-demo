6ff3495f69d727fc1f4cec45c950903b154daa50
po.cheng <po.cheng@adlinktech.com>
2022-07-06 16:49:50 +0800

    multiconfig: update bash script to setup multiconfig for various machine architecture
    Signed-off-by: po.cheng <po.cheng@adlinktech.com>

--- conf/setup-multiconfig.sh
+++ conf/setup-multiconfig.sh
@@ -3,16 +3,25 @@
 TARGET=${1:-genericx86-64}
 
 if [ -f ${BUILDDIR}/conf/local.conf ]; then
-  if ! grep -q "^BBMULTICONFIG" $BUILDDIR/conf/local.conf; then
+  if ! grep -q "^BBMULTICONFIG" ${BUILDDIR}/conf/local.conf; then
+    echo "# setup BBMULTICONFIG in local.conf for conf/multiconfig/${TARGET}.conf" | tee -a ${BUILDDIR}/conf/local.conf
+    echo "BBMULTICONFIG = \"${TARGET}\"" >> ${BUILDDIR}/conf/local.conf
+  else
+    echo "BBMULTICONFIG already defined in local.conf"
+  fi
+  if [ ! -f ${BUILDDIR}/conf/multiconfig/${TARGET}.conf ]; then
     mkdir -p ${BUILDDIR}/conf/multiconfig
     cat > ${BUILDDIR}/conf/multiconfig/${TARGET}.conf << EOF
 MACHINE = "${TARGET}"
 TMPDIR = "\${TOPDIR}/tmp-${TARGET}"
 EOF
-    echo "# setup BBMULTICONFIG in local.conf for conf/multiconfig/${TARGET}.conf" | tee -a ${BUILDDIR}/conf/local.conf
-    echo "BBMULTICONFIG = \"${TARGET}\"" >> ${BUILDDIR}/conf/local.conf
-  else
-    echo "BBMULTICONFIG already defined in local.conf"
+  fi
+  if [ -f ${BUILDDIR}/conf/multiconfig/${TARGET}.conf ]; then
+	if [ "${MACHINE}" = "arm-container" ]; then
+		echo "DISTRO = \"poky\"" >> ${BUILDDIR}/conf/multiconfig/${TARGET}.conf
+		echo "DISTRO_FEATURES_append = \" virtualization\"" >> ${BUILDDIR}/conf/multiconfig/${TARGET}.conf
+		echo "IMAGE_COMPRESS_TYPE ?= \"tar.gz\"" >> ${BUILDDIR}/conf/multiconfig/${TARGET}.conf
+	fi
   fi
 else
   echo "No local.conf found"
